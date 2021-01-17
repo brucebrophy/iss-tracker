c53bd170bab463df5dcb5241815db07b8d3b541f
Bruce Brophy <bruce@brucebrophy.com>
2021-01-17 17:26:22 -0500

    Updates Kiosk start up script

--- kiosk.sh
+++ kiosk.sh
@@ -8,8 +8,8 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:3000
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:3000 &
 
-git -C ./Desktop/iss-tracker pull
+git -C ./Desktop/iss-tracker pull 
 npm --prefix ./Desktop/iss-tracker install ./Desktop/iss-tracker
-npm --prefix ./Desktop/iss-tracker run start ./Desktop/iss-tracker
+npm --prefix ./Desktop/iss-tracker run start ./Desktop/iss-tracker & 
