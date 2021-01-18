0c79d15ffcec933bc78ab5451d79197eb990851d
Bruce Brophy <bruce@brucebrophy.com>
2021-01-17 22:12:58 -0500

    Updates Kiosk Bash Scripts & Updates Default Zoom

--- kiosk.sh
+++ kiosk.sh
@@ -10,8 +10,8 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:3000
+git -C ./Desktop/iss-tracker pull &&
+npm --prefix ./Desktop/iss-tracker install ./Desktop/iss-tracker &&
+npm --prefix ./Desktop/iss-tracker run start ./Desktop/iss-tracker &
 
-git -C ./Desktop/iss-tracker pull 
-npm --prefix ./Desktop/iss-tracker install ./Desktop/iss-tracker
-npm --prefix ./Desktop/iss-tracker run start ./Desktop/iss-tracker & 
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:3000 &
--- src/components/Maps/Map.js
+++ src/components/Maps/Map.js
@@ -7,7 +7,7 @@ export default function Map({ mapCallback }) {
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_KEY }}
         defaultCenter={[43.653, -79.383]}
-        defaultZoom={0}
+        defaultZoom={3}
         onGoogleApiLoaded={({ map }) => mapCallback(map)}
         yesIWantToUseGoogleMapApiInternals
         options={{
