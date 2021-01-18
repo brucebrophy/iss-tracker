ebf05a896be00acceb73fd5b861b6436cfbb5949
Bruce Brophy <bruce@brucebrophy.com>
2021-01-17 22:43:29 -0500

    Updates Bash Script

--- kiosk.sh
+++ kiosk.sh
@@ -10,8 +10,6 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-git -C ./Desktop/iss-tracker pull &&
-npm --prefix ./Desktop/iss-tracker install ./Desktop/iss-tracker &&
-npm --prefix ./Desktop/iss-tracker run start ./Desktop/iss-tracker &
-
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:3000 &
+
+npm --prefix ./Desktop/iss-tracker run start ./Desktop/iss-tracker
